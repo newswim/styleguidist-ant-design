942c392f7721678922f0e4e26760484da15fffef
Dan Minshew <ofenixculpa@gmail.com>
2018-06-13 18:41:32 -0500

    move comment and add more context

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,9 +2,13 @@ const theme = require('./theme.js')
 const { injectBabelPlugin } = require('react-app-rewired')
 const rewireLess = require('react-app-rewire-less')
 
+// NOTE: ideally this would load directly from here:
+// { components: './node_modules/antd/es/**/index.js' }
+// However, for now I'm just manually copying the components and props
+// hence, not all of them are available.
+
 module.exports = {
   styleguideDir: 'docs',
-  // components: './node_modules/antd/es/**/index.js',
   components: 'src/components/**/*.js',
   dangerouslyUpdateWebpackConfig(webpackConfig, env) {
     webpackConfig = injectBabelPlugin(
